7bd91d5a49929d5109bc2eaa4838f1f134b7e36f
Matvey Androsyuk <matveyandrosyuk2002@mail.ru>
2022-01-08 20:42:48 +0300

    all files were added

--- src/App.js
+++ src/App.js
@@ -1,39 +1,59 @@
-import React, {useEffect, useState} from "react";
+import React, {useState} from "react";
 import "./styles/App.css"
 import PostList from "./components/PostList";
-import MyButton from "./components/UI/button/MyButton";
-import MyInput from "./components/UI/input/MyInput";
-import PostService from "./service/PostService";
+import PostForm from "./components/PostForm";
+import MySelect from "./components/UI/select/MySelect";
 
 
 function App() {
-    const [posts, setPosts] = useState([])
+    const [posts, setPosts] = useState([
+        {id: 1, title: 'Java', body: 'Description'},
+        {id: 2, title: 'JavaScript', body: 'Description'},
+        {id: 3, title: 'HTML', body: 'Description'}
+    ])
 
-    useEffect(() => {
+    const [selectedSort, setSelectedSort] = useState('')
+
+    /*useEffect(() => {
         PostService.getPosts().then((res) => {
             setPosts(res.data)
         })
-    })
+    })*/
 
-    const [title, setTitle] = useState('')
+    const sortPosts = (sort) => {
+        setSelectedSort(sort)
+        setPosts([...posts].sort((a, b) => a[sort].localeCompare(b[sort])))
+    }
 
-    const addNewPost = (e) => {
-        e.preventDefault()
+    const createPost = (newPost) => {
+        setPosts([...posts, newPost])
+    }
+
+    const deletePost = (post) => {
+        setPosts(posts.filter(p => p.id !== post.id))
     }
 
   return (
       <div className={'App'}>
-      <form>
-          <MyInput
-              value = {title}
-              onChange={event => setTitle(event.target.value)}
-              type='text'
-              placeholder='Название поста'
+          <PostForm create={createPost}/>
+
+          <hr style={{margin : '15px, 0'}}/>
+
+          <MySelect
+              value={selectedSort}
+              onChange={sortPosts}
+              defaultValue={"Сортировать по"}
+              options={[{value: 'title', name: 'По названию'},
+                        {value: 'body', name: 'По описанию'}]}
           />
-          <MyInput type='text' placeholder={'Описание поста'}/>
-          <MyButton onClick={addNewPost}>Создать пост</MyButton>
-      </form>
-      <PostList posts={posts} title={"Список постов"}/>
+          {posts.length !== 0
+            ?
+              <PostList remove={deletePost} posts={posts} title={"Список постов"}/>
+            :
+              <h1 style={{textAlign: 'center'}}>
+                  Посты не найдены!
+              </h1>}
+
       </div>
   );
 }
